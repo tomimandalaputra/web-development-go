9e54fb77a1e56daef6ac39f0c4dd1f76cb2883f2
tomimandalaputra <tommymandalaputra@gmail.com>
2026-01-27 14:26:38 +0700

    create a form processing library

---
+++ forms.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+type errors map[string][]string
+
+func (e errors) Add(field, message string) {
+	e[field] = append(e[field], message)
+}
+
+func (e errors) Get(field string) string {
+	fields, ok := e[field]
+	if !ok {
+		return ""
+	}
+	return fields[0]
+}
+
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+func NewForm(form url.Values) *Form {
+	return &Form{
+		Values: form,
+		Errors: errors(map[string][]string{}),
+	}
+}
+
+func (f *Form) Required(fields ...string) *Form {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			label := []rune(field)
+			label[0] = unicode.ToUpper(label[0])
+			f.Errors.Add(field, fmt.Sprintf("%s is required", string(label)))
+		}
+	}
+	return f
+}
+
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
+func (f *Form) MinLength(field string, n int) *Form {
+	value := f.Get(field)
+	if value == "" {
+		return f
+	}
+
+	if utf8.RuneCountInString(value) < n {
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum of %d characters)", n))
+	}
+
+	return f
+}
+
+func (f *Form) MaxLength(field string, n int) *Form {
+	value := f.Get(field)
+	if value == "" {
+		return f
+	}
+
+	if utf8.RuneCountInString(value) > n {
+		f.Errors.Add(field, fmt.Sprintf("This field is too long (maximum of %d characters)", n))
+	}
+
+	return f
+}
+
+func (f *Form) Matches(field string, pattern *regexp.Regexp) *Form {
+	value := f.Get(field)
+	if value == "" {
+		return f
+	}
+
+	if !pattern.MatchString(value) {
+		f.Errors.Add(field, "This field is invalid")
+	}
+
+	return f
+}
