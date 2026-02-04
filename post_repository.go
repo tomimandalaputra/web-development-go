89b6a2379d3fc0e8f442b8af87f26db9454d1121
tomimandalaputra <tommymandalaputra@gmail.com>
2026-02-04 10:25:20 +0700

    implementation post repository

--- post_repository.go
+++ post_repository.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"math"
+	"strings"
 	"time"
+)
+
+var (
+	ErrDuplicatePostTitle = errors.New("Duplicate post title ")
+	ErrDuplicateVote      = errors.New("Duplicate vote ")
 )
 
 type Post struct {
@@ -85,8 +92,104 @@ func calculateMetadata(totalRecords, page, pageSize int) Metadata {
 type PostRepository interface {
 	CreatePost(title, url string, userID int) (int, error)
 	AddComment(userID, postID int, body string) (int, error)
-	AddVote(userID, postID int, body string) error
+	AddVote(userID, postID int) error
 	GetAll(filter Filter) ([]Post, Metadata, error)
 	GetByID(id int) (*Post, error)
 	GetComments(postID int) ([]Comment, error)
+}
+
+type SQLPostRepository struct {
+	db *sql.DB
+}
+
+// NewSQLPostRepository create new SQLPostRepository type
+func NewSQLPostRepository(db *sql.DB) *SQLPostRepository {
+	return &SQLPostRepository{
+		db: db,
+	}
+}
+
+func (r *SQLPostRepository) CreatePost(title, url string, userID int) (int, error) {
+	stmt := "INSERT INTO posts (title, url, user_id) VALUES (?,?,?)"
+
+	result, err := r.db.Exec(stmt, title, url, userID)
+	if err != nil {
+		if strings.Contains(err.Error(), "UNIQUE constraint failed: post.title") {
+			return 0, ErrDuplicatePostTitle
+		}
+		return 0, err
+	}
+
+	postID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(postID), nil
+}
+
+func (r *SQLPostRepository) AddComment(userID, postID int, body string) (int, error) {
+	stmt := "INSERT INTO comments (user_id, post_id, body) VALUES (?,?,?)"
+
+	result, err := r.db.Exec(stmt, userID, postID, body)
+	if err != nil {
+		return 0, err
+	}
+
+	commentID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(commentID), nil
+}
+
+func (r *SQLPostRepository) AddVote(userID, postID int) error {
+	stmt := "INSERT INTO votes (user_id, post_id) VALUES (?, ?)"
+
+	_, err := r.db.Exec(stmt, userID, postID)
+	if err != nil {
+		if strings.Contains(err.Error(), "UNIQUE constraint failed") || strings.Contains(err.Error(), "PRIMARY KEY constraint failed") {
+			return ErrDuplicateVote
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func (r *SQLPostRepository) GetByID(id int) (*Post, error) {
+	query := `
+		SELECT p.id, p.title, p.url, p.user_id, p.created_at,
+		u.name as user_name,
+		COUNT(DISTINCT c.id) AS comment_count,
+		COUNT(DISTINCT v.user_id) AS voute_count
+		FROM posts p
+		LEFT JOIN users u ON p.user_id = u.id
+		LEFT JOIN comments c ON p.id = c.post_id
+		LEFT JOIN votes v ON p.id = v.post_id
+		WHERE p.id = ?
+		GROUP BY p.id, p.title, p.url, p.user_id, p.created_at, u.name
+	`
+
+	row := r.db.QueryRow(query, id)
+
+	var post Post
+	err := row.Scan(
+		&post.ID,
+		&post.Title,
+		&post.URL,
+		&post.UserID,
+		&post.CreatedAt,
+		&post.UserName,
+		&post.CommentCount,
+		&post.VoteCount,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &post, nil
 }
