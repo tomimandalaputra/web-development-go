004a4d190133fe50b19ba218ae4b5864ba7c9ab3
tomimandalaputra <tommymandalaputra@gmail.com>
2026-02-12 13:19:46 +0700

    testing the logger middleware

---
+++ middlewares_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogg(t *testing.T) {
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	buf := new(bytes.Buffer)
+	testApp.infoLog = log.New(buf, "", 0)
+
+	handler := testApp.logger(testHandler)
+	req := httptest.NewRequest(http.MethodGet, "/test", nil)
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "OK", w.Body.String())
+	assert.Contains(t, buf.String(), "HTTP/1.1 GET /test")
+}
